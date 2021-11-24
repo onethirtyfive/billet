72143aba5509c791f5e7dd7c4e188eb2a8773d19
Joshua Morris <joshua.a.morris@gmail.com>
2021-11-23 21:30:17 -0600

    improvements: facades, producers, calling code

--- script.js
+++ script.js
@@ -1,51 +1,16 @@
 import { readFileSync } from 'fs'
+import { modify } from './lib/producers.mjs'
+import { fronting } from './lib/facades.mjs'
 
-import producing from './lib/producers.mjs'
+const persistedLog = JSON.parse(readFileSync('log.json'))
 
-let log
+const log = modify(persistedLog)
+  .define('joel')
+  .define('joshua')
+  .realias('joel', 'stephanie')
+  .relate('joshua', 'stephanie', "$match(event,'gavePresent')")
+  .freeze()
 
-log = JSON.parse(readFileSync('log.json'))
+const facade = fronting(log)
 
-log = producing(log)
-log = log.defineTopic('foo')
-log = log.defineTopic('bar')
-log = log.linkDownstreamTopic('foo', 'bar', 'true')
-log = log.unlinkDownstreamTopic('foo', 'bar')
-log = log.realiasTopic('foo', 'newTopic')
-
-console.log(JSON.stringify(log, null, 2))
-
-// console.log(JSON.stringify(log, null, 2))
-
-// log = ['joel', 'joshua', 'chiquita'].reduce(
-//   (modifying, topic) => modifying.define(topic),
-//   log
-// ).result
-
-// console.log(JSON.stringify(log, null, 2))
-
-// log = modifyDownstreams(log)
-//   .link('joel', 'joshua', '$true')
-//   .unlink('joel', 'joshua')
-//   .result
-
-// console.log(JSON.stringify(log, null, 2))
-
-// // need to make a plan
-
-// log = modifyEntries(log)
-//   .append(
-//     {
-//       timestamp: 1632974404123,
-//       event: 'anticipation.create',
-//       context: {
-//         uuid: '123e4567-e89b-12d3-a456-426614174000',
-//         attributes: {
-//           name: 'Lindsey Repayment',
-//           wealthEffect: 'gain',
-//           eventuality: 'imminent',
-//           amount: 1234.56
-//         }
-//       }
-//     }
-//   )
+console.log(facade.topicFor('stephanie'))
