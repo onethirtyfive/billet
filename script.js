c3288bfed4b0a48911c55324dfb15c367354b175
Joshua Morris <joshua.a.morris@gmail.com>
2021-12-11 14:06:36 -0600

    switch to md5 (checksum, not digest); log changes; append functionality

--- script.js
+++ script.js
@@ -1,49 +1,55 @@
 import { readFileSync } from 'fs'
 import { enableMapSet } from 'immer'
-import { mkLOM } from './lib/support/model.mjs'
-import { changing } from './lib/support/modification.mjs'
-import { mkTopics } from './lib/support/topics.mjs'
-import { graph } from './lib/graph.mjs'
-import { make } from './lib/log.mjs'
+
+import { loadLog } from './lib/log.mjs'
 
 enableMapSet()
 
-const persisted = JSON.parse(readFileSync('log.json'))
+const data = JSON.parse(readFileSync('log.json'))
 
-const lom = changing(mkLOM(persisted))
+let log = loadLog(data)
+  .revising()
   .define('joel')
   .define('joshua')
   .realias('joel', 'stephanie')
   .relate('root', 'joshua', 'false')
   .relate('joshua', 'stephanie', 'false')
-  .freeze()
+  .done()
+const { state, topics, graph } = log
 
-console.log(lom)
-
-const topics = mkTopics(lom)
-const _graph = graph(topics)
-const log = make(_graph, lom.entries)
+console.log('\n\n*** Log ***\n\n')
+state.aliases.forEach((uuid, alias) => {
+  console.log(alias)
+  console.log(topics.topicAt(uuid))
+  console.log()
+})
 
 console.log('\n\n*** GRAPH NODES ***\n\n')
-for (const [value, node] of _graph.nodes) {
+for (const [value, node] of graph.nodes) {
   const fnGetUUID = node => node.value.uuid
   console.log(value.uuid)
   console.log(`adjacents: ${node.getAdjacents().map(fnGetUUID).join(', ')}`)
   console.log()
 }
 
-console.log('\n\n*** LOM ***\n\n')
-lom.aliases.forEach((uuid, alias) => {
-  console.log(alias)
-  console.log(topics.topicAt(uuid))
-  console.log()
-})
-
 console.log('\n\n*** EVENT PLANNING (GRAPH TRAVERSAL) ***\n')
-const event = { event: 'anticipations.foo' }
-console.log(`traversing with: ${JSON.stringify(event, null, 2)}\n`)
-const rootNode = _graph.nodes.get(topics.rootTopic)
-const plan = log.plan(rootNode, event)
 
+const event = { uuid: uuidv4(), event: 'anticipations.foo', context: {} }
+console.log(`traversing with: ${JSON.stringify(event, null, 2)}\n`)
+
+const rootNode = graph.nodes.get(topics.rootTopic)
+const plan = log.plan(event)
 console.log('plan:')
-console.log(plan.map(node => node.value.uuid))
+console.log(plan.map(topic => topic.uuid))
+
+import { v4 as uuidv4 } from 'uuid'
+
+log = log
+  .revising()
+  .append(plan, event)
+  .done()
+
+const { state: { universe } } = log
+console.log(universe)
+
+console.log(log.state.caches.get('1e3c5d1c-b9db-43fa-a3f6-29f46821d5f6'))
