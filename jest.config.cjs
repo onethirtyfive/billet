7b3e955206cfe026d357f6b5b1d61361893d198e
Joshua Morris <joshua.a.morris@gmail.com>
2021-12-22 22:56:21 -0600

    misc project settings

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest/presets/default-esm',
+  modulePaths: [
+    "<rootDir>/src/"
+  ],
   testPathIgnorePatterns: [
     "dist"
   ],
